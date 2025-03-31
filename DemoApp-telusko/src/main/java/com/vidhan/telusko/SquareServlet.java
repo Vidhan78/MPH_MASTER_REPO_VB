2e88687d0cdddfa8c18ac108be197fb40fc57349
Vidhan Bhasme <vidhan.bhasme@mphasis.com>
2025-03-31 17:56:42 +0530

    teluskolearn

---
+++ DemoApp-telusko/src/main/java/com/vidhan/telusko/SquareServlet.java
@@ -0,0 +1,17 @@
+package com.vidhan.telusko;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SquareServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	 {
+		PrintWriter out=res.getWriter();
+	     out.println("Hello to sq");		
+	 }
+}
